06bfbc240f53f5d65942d14aa776a3af08733992
geetanjali-mali-pst <geetanjali.mali@positsource.com>
2025-02-26 15:01:40 +0530

    Added data table and pdf preview onclick to view mode

--- front/src/pages/Agreements.tsx
+++ front/src/pages/Agreements.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+
+import { DataTable } from 'mantine-datatable';
+import { Box, ActionIcon } from '@mantine/core';
+import { companies } from './Data';
+import { IconEye } from '@tabler/icons-react';
+import * as ReactPDF from '@react-pdf/renderer';
+import { MyDocument } from './useModal';
 
 export function Agreements() {
+  const handleViewPDF = async () => {
+    const blob = await ReactPDF.pdf(<MyDocument />).toBlob();
+    const url = URL.createObjectURL(blob);
+    window.open(url, '_blank'); 
+  };
+
   return (
-    <div>
-      <h1>Agreement  Page</h1>
-    </div>
+      <DataTable
+      withTableBorder
+      striped
+      highlightOnHover
+        style={{ width:'55rem' }}
+        columns={[
+          { accessor: 'name', textAlign: 'center', width: 180 , title: <Box ml={100}>Agreement Name</Box> },
+            { accessor: 'city', textAlign: 'center', width: 100,title: <Box ml={80}>City</Box>  },
+          { accessor: 'state', textAlign: 'center', width: 80,title: <Box ml={50}>State</Box> },
+           { accessor: 'actions',width: 80 ,
+            title: <Box mr={50}>Row actions</Box>,
+            textAlign: 'center',
+            render: () => (
+                <ActionIcon
+                  size="sm"
+                  variant="subtle"
+                  color="green"
+                  onClick={handleViewPDF}
+                >
+                  <IconEye size={16} />
+                </ActionIcon>
+
+            ),
+          },
+        ]}
+        records={companies}
+      />
+
   );
 }
---
+++ front/src/pages/Data.ts
@@ -0,0 +1,75 @@
+export const companies = [
+    {
+        id: "8",
+        name: "Agreement-1",
+        streetAddress: "21716 Ratke Drive",
+        city: "Stromanport",
+        state: "WY",
+        missionStatement: "Innovate bricks-and-clicks metrics."
+    },
+    {
+        id: "9",
+        name: "Feest, Bogan and Herzog",
+        streetAddress: "21716 Ratke Drive",
+        city: "Stromanport",
+        state: "WY",
+        missionStatement: "Innovate bricks-and-clicks metrics."
+    },
+    {
+        id: "1",
+        name: "Feest, Bogan and Herzog",
+        streetAddress: "21716 Ratke Drive",
+        city: "Stromanport",
+        state: "WY",
+        missionStatement: "Innovate bricks-and-clicks metrics."
+    },
+    {
+        id: "2",
+        name: "Feest, Bogan and Herzog",
+        streetAddress: "21716 Ratke Drive",
+        city: "Stromanport",
+        state: "WY",
+        missionStatement: "Innovate bricks-and-clicks metrics."
+    },
+    {
+        id: "3",
+        name: "Feest, Bogan and Herzog",
+        streetAddress: "21716 Ratke Drive",
+        city: "Stromanport",
+        state: "WY",
+        missionStatement: "Innovate bricks-and-clicks metrics."
+    },
+    {
+    id: "4",
+    name: "Feest, Bogan and Herzog",
+    streetAddress: "21716 Ratke Drive",
+    city: "Stromanport",
+    state: "WY",
+    missionStatement: "Innovate bricks-and-clicks metrics."
+  },
+  {
+    id: "5",
+    name: "Feest, Bogan and Herzog",
+    streetAddress: "21716 Ratke Drive",
+    city: "Stromanport",
+    state: "WY",
+    missionStatement: "Innovate bricks-and-clicks metrics."
+  },
+  {
+    id: "6",
+    name: "Feest, Bogan and Herzog",
+    streetAddress: "21716 Ratke Drive",
+    city: "Stromanport",
+    state: "WY",
+    missionStatement: "Innovate bricks-and-clicks metrics."
+  },
+  {
+    id: "7",
+    name: "Feest, Bogan and Herzog",
+    streetAddress: "21716 Ratke Drive",
+    city: "Stromanport",
+    state: "WY",
+    missionStatement: "Innovate bricks-and-clicks metrics."
+  }
+
+];
