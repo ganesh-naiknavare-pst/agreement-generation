0c97ddfb732a736160758fdc671f8a20fec8fea3
geetanjali-mali-pst <geetanjali.mali@positsource.com>
2025-02-27 13:31:27 +0530

    Incorporated feedbacks

--- front/src/pages/Agreements.tsx
+++ front/src/pages/Agreements.tsx
@@ -1,52 +1,77 @@
-import { ActionIcon, Table, Text, Center } from '@mantine/core';
-import { companies } from './Data';
+import { ActionIcon, Table, Text, Center, Badge, Tooltip } from '@mantine/core';
+import { agreements } from './Data';
 import { IconEye } from '@tabler/icons-react';
 import * as ReactPDF from '@react-pdf/renderer';
 import { MyDocument } from './useModal';
+import { COLORS } from '../colors';
 
 export function Agreements() {
   const handleViewPDF = async () => {
     const blob = await ReactPDF.pdf(<MyDocument />).toBlob();
     const url = URL.createObjectURL(blob);
-    window.open(url, '_blank'); 
+    window.open(url, '_blank');
   };
 
-  console.log('Companies:', Array.isArray(companies));
+  const getStatusColor = (status: string) => {
+    switch (status) {
+      case "Waiting approval from Owner and Tenant":
+        return COLORS.yellow;
+      case "Waiting approval from Owner":
+        return COLORS.yellow;
+      case "Waiting approval from Tenant":
+        return COLORS.yellow;
+      case "APPROVED":
+        return COLORS.approval;
+
+    }
+  };
 
   return (
-      <Table striped highlightOnHover withTableBorder>
-        <Table.Thead style={{ backgroundColor: '#f5f5f5' }}>
-          <Table.Tr >
-            <Table.Th>Agreement name</Table.Th>
-            <Table.Th>Address</Table.Th>
-            <Table.Th>Date</Table.Th>
-            <Table.Th>Action</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {companies.length > 0 ? (
-            companies.map((company) => (
-              <Table.Tr key={company.id}>
-                <Table.Td style={{ textAlign: 'left' }}>{company.name}</Table.Td>
-                <Table.Td style={{ textAlign: 'left' }}>{`${company.streetAddress}, ${company.city}, ${company.state}`}</Table.Td>
-                <Table.Td style={{ textAlign: 'left' }}>N/A</Table.Td>
-                <Table.Td style={{ textAlign: 'left' }}>
-                  <ActionIcon onClick={handleViewPDF}>
-                    <IconEye />
-                  </ActionIcon>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%' }}>
+        <Table striped highlightOnHover withTableBorder verticalSpacing="md" horizontalSpacing={20}>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>Agreement Title</Table.Th>
+              <Table.Th>Owner Name</Table.Th>
+              <Table.Th>Tenant Name</Table.Th>
+              <Table.Th>Created Time</Table.Th>
+              <Table.Th>Status</Table.Th>
+              <Table.Th>Actions</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {agreements.length > 0 ? (
+              agreements.map((agreement) => (
+                <Table.Tr key={agreement.id}>
+                  <Table.Td style={{ textAlign: 'left' }}>{agreement.title}</Table.Td>
+                  <Table.Td style={{ textAlign: 'left' }}>{agreement.ownerName}</Table.Td>
+                  <Table.Td style={{ textAlign: 'left' }}>{agreement.tenantName}</Table.Td>
+                  <Table.Td style={{ textAlign: 'left' }}>{agreement.createdTime}</Table.Td>
+                  <Table.Td style={{ textAlign: 'left' }}>
+                   <Tooltip label={agreement.status} withArrow>
+                     <Badge size="sm" color={getStatusColor(agreement.status)}>
+                      {agreement.status}
+                     </Badge>
+                   </Tooltip>
+                  </Table.Td>
+                  <Table.Td style={{ textAlign: 'left' }}>
+                    <ActionIcon onClick={handleViewPDF}>
+                      <IconEye />
+                    </ActionIcon>
+                  </Table.Td>
+                </Table.Tr>
+              ))
+            ) : (
+              <Table.Tr>
+                <Table.Td colSpan={6}>
+                  <Center py="md">
+                    <Text c="dimmed">No data found</Text>
+                  </Center>
                 </Table.Td>
               </Table.Tr>
-            ))
-          ) : (
-            <Table.Tr>
-              <Table.Td colSpan={4}>
-                <Center py="md">
-                  <Text c="dimmed">No data found</Text>
-                </Center>
-              </Table.Td>
-            </Table.Tr>
-          )}
-        </Table.Tbody>
-      </Table>
+            )}
+          </Table.Tbody>
+        </Table>
+    </div>
   );
 }
--- front/src/pages/Data.ts
+++ front/src/pages/Data.ts
@@ -1,75 +1,65 @@
-export const companies = [
+
+export const agreements = [
     {
-        id: "8",
-        name: "Agreement-1",
-        streetAddress: "21716 Ratke Drive",
-        city: "Stromanport",
-        state: "WY",
-        missionStatement: "Innovate bricks-and-clicks metrics."
+        id: "1",
+        title: "Agreement-1",
+        ownerName: "Alice Johnson",
+        tenantName: "Bob Brown",
+        createdTime: "2025-02-26",
+        status: "APPROVED",
+       
     },
     {
         id: "9",
-        name: "Feest, Bogan and Herzog",
-        streetAddress: "21716 Ratke Drive",
-        city: "Stromanport",
-        state: "WY",
-        missionStatement: "Innovate bricks-and-clicks metrics."
+        title: "Rent Agreement-1",
+        ownerName: "Alice Johnson",
+        tenantName: "Bob Brown",
+        createdTime: "2025-02-26",
+        status: "Waiting approval from Owner and Tenant",
+       
     },
     {
-        id: "1",
-        name: "Feest, Bogan and Herzog",
-        streetAddress: "21716 Ratke Drive",
-        city: "Stromanport",
-        state: "WY",
-        missionStatement: "Innovate bricks-and-clicks metrics."
+        id: "9",
+        title: "Feest, Bogan and Herzog",
+        ownerName: "Alice Johnson",
+        tenantName: "Bob Brown",
+        createdTime: "2025-02-26",
+        status: "Waiting approval from Owner",
     },
     {
-        id: "2",
-        name: "Feest, Bogan and Herzog",
-        streetAddress: "21716 Ratke Drive",
-        city: "Stromanport",
-        state: "WY",
-        missionStatement: "Innovate bricks-and-clicks metrics."
+        id: "9",
+        title: "Feest, Bogan and Herzog",
+        ownerName: "Alice Johnson",
+        tenantName: "Bob Brown",
+        createdTime: "2025-02-26",
+        status: "Waiting approval from Tenant",
     },
     {
-        id: "3",
-        name: "Feest, Bogan and Herzog",
-        streetAddress: "21716 Ratke Drive",
-        city: "Stromanport",
-        state: "WY",
-        missionStatement: "Innovate bricks-and-clicks metrics."
+        id: "9",
+        title: "Feest, Bogan and Herzog",
+        ownerName: "Alice Johnson",
+        tenantName: "Bob Brown",
+        createdTime: "2025-02-26",
+        status: "APPROVED",
+       
     },
     {
-    id: "4",
-    name: "Feest, Bogan and Herzog",
-    streetAddress: "21716 Ratke Drive",
-    city: "Stromanport",
-    state: "WY",
-    missionStatement: "Innovate bricks-and-clicks metrics."
-  },
-  {
-    id: "5",
-    name: "Feest, Bogan and Herzog",
-    streetAddress: "21716 Ratke Drive",
-    city: "Stromanport",
-    state: "WY",
-    missionStatement: "Innovate bricks-and-clicks metrics."
-  },
-  {
-    id: "6",
-    name: "Feest, Bogan and Herzog",
-    streetAddress: "21716 Ratke Drive",
-    city: "Stromanport",
-    state: "WY",
-    missionStatement: "Innovate bricks-and-clicks metrics."
-  },
-  {
-    id: "7",
-    name: "Feest, Bogan and Herzog",
-    streetAddress: "21716 Ratke Drive",
-    city: "Stromanport",
-    state: "WY",
-    missionStatement: "Innovate bricks-and-clicks metrics."
-  }
+        id: "9",
+        title: "Feest, Bogan and Herzog",
+        ownerName: "Alice Johnson",
+        tenantName: "Bob Brown",
+        createdTime: "2025-02-26",
+        status: "APPROVED",
+        
+    },
+    {
+        id: "9",
+        title: "Feest, Bogan and Herzog",
+        ownerName: "Alice Johnson",
+        tenantName: "Bob Brown",
+        createdTime: "2025-02-26",
+        status: "Waiting approval from Owner and Tenant",
+ 
+    },
 
 ];
