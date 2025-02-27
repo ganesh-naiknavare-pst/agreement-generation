e18f13826b06ed2b046b8cacdbe56443dfbe90d7
geetanjali-mali-pst <geetanjali.mali@positsource.com>
2025-02-27 10:28:28 +0530

    Added datatable and pdf preview for Agreement details

--- front/src/pages/Agreements.tsx
+++ front/src/pages/Agreements.tsx
@@ -1,6 +1,4 @@
-
-import { DataTable } from 'mantine-datatable';
-import { Box, ActionIcon } from '@mantine/core';
+import { ActionIcon, Table, Text, Center } from '@mantine/core';
 import { companies } from './Data';
 import { IconEye } from '@tabler/icons-react';
 import * as ReactPDF from '@react-pdf/renderer';
@@ -13,34 +11,42 @@ export function Agreements() {
     window.open(url, '_blank'); 
   };
 
+  console.log('Companies:', Array.isArray(companies));
+
   return (
-      <DataTable
-      withTableBorder
-      striped
-      highlightOnHover
-        style={{ width:'55rem' }}
-        columns={[
-          { accessor: 'name', textAlign: 'center', width: 180 , title: <Box ml={100}>Agreement Name</Box> },
-            { accessor: 'city', textAlign: 'center', width: 100,title: <Box ml={80}>City</Box>  },
-          { accessor: 'state', textAlign: 'center', width: 80,title: <Box ml={50}>State</Box> },
-           { accessor: 'actions',width: 80 ,
-            title: <Box mr={50}>Row actions</Box>,
-            textAlign: 'center',
-            render: () => (
-                <ActionIcon
-                  size="sm"
-                  variant="subtle"
-                  color="green"
-                  onClick={handleViewPDF}
-                >
-                  <IconEye size={16} />
-                </ActionIcon>
-
-            ),
-          },
-        ]}
-        records={companies}
-      />
-
+      <Table striped highlightOnHover withTableBorder>
+        <Table.Thead style={{ backgroundColor: '#f5f5f5' }}>
+          <Table.Tr >
+            <Table.Th>Agreement name</Table.Th>
+            <Table.Th>Address</Table.Th>
+            <Table.Th>Date</Table.Th>
+            <Table.Th>Action</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {companies.length > 0 ? (
+            companies.map((company) => (
+              <Table.Tr key={company.id}>
+                <Table.Td style={{ textAlign: 'left' }}>{company.name}</Table.Td>
+                <Table.Td style={{ textAlign: 'left' }}>{`${company.streetAddress}, ${company.city}, ${company.state}`}</Table.Td>
+                <Table.Td style={{ textAlign: 'left' }}>N/A</Table.Td>
+                <Table.Td style={{ textAlign: 'left' }}>
+                  <ActionIcon onClick={handleViewPDF}>
+                    <IconEye />
+                  </ActionIcon>
+                </Table.Td>
+              </Table.Tr>
+            ))
+          ) : (
+            <Table.Tr>
+              <Table.Td colSpan={4}>
+                <Center py="md">
+                  <Text c="dimmed">No data found</Text>
+                </Center>
+              </Table.Td>
+            </Table.Tr>
+          )}
+        </Table.Tbody>
+      </Table>
   );
 }
