29a9ddab74c1db7a9da8470ec78630923e9e4279
vaishnavi-more-pst <vaishnavi@positsource.com>
2025-02-27 16:34:58 +0530

    Removed comments

--- front/src/pages/AgreementGenerator.tsx
+++ front/src/pages/AgreementGenerator.tsx
@@ -147,14 +147,14 @@ export function AgreementGenerator() {
       rent_amount: "1500",
       start_date: form.values.date.toISOString().split("T")[0],
     };
-    // try {
-    //   await fetchData({
-    //     method: "POST",
-    //     data: requestData,
-    //   });
-    // } catch (error) {
-    //   console.error("Error creating agreement:", error);
-    // }
+    try {
+      await fetchData({
+        method: "POST",
+        data: requestData,
+      });
+    } catch (error) {
+      console.error("Error creating agreement:", error);
+    }
   };
 
   return (
