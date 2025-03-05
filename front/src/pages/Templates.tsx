2f3d92372f1f1feb374eaa26d1e75f2dc006b8d4
vaishnavi-more-pst <vaishnavi@positsource.com>
2025-03-05 18:17:41 +0530

    Added title to template page

--- front/src/pages/Templates.tsx
+++ front/src/pages/Templates.tsx
@@ -11,6 +11,8 @@ import {
   Card,
   ThemeIcon,
   useMantineColorScheme,
+  Title,
+  Divider,
 } from "@mantine/core";
 import { Dropzone, MIME_TYPES } from "@mantine/dropzone";
 import {
@@ -77,111 +79,122 @@ export function Templates() {
   };
 
   return (
-    <Box style={{ maxWidth: 500, margin: "auto", padding: "2rem" }}>
-      {!displayBanner ? (
-        <>
-          {message && (
-            <Notification
-              color={message.type === "success" ? "teal" : "red"}
-              icon={
-                message.type === "success" ? (
-                  <IconCheck />
-                ) : (
-                  <IconAlertTriangle />
-                )
-              }
-              mb="md"
+    <>
+      <Group align="center">
+        <Title order={3} mb={20}>
+          Generate an Agreement by Uploading Templates
+        </Title>
+      </Group>
+      <Divider my="sm" />
+      <Box style={{ maxWidth: 700, margin: "auto", padding: "2rem" }}>
+        {!displayBanner ? (
+          <>
+            {message && (
+              <Notification
+                color={message.type === "success" ? "teal" : "red"}
+                icon={
+                  message.type === "success" ? (
+                    <IconCheck />
+                  ) : (
+                    <IconAlertTriangle />
+                  )
+                }
+                mb="md"
+              >
+                {message.text}
+              </Notification>
+            )}
+
+            <Dropzone
+              onDrop={handleDrop}
+              accept={[MIME_TYPES.pdf, MIME_TYPES.doc, MIME_TYPES.docx]}
             >
-              {message.text}
-            </Notification>
-          )}
+              <Group align="center" gap="xl">
+                <IconUpload size={50} />
+                <Text>Drag a file here or click to upload</Text>
+              </Group>
+            </Dropzone>
 
-          <Dropzone onDrop={handleDrop} accept={[MIME_TYPES.pdf, MIME_TYPES.doc, MIME_TYPES.docx]}>
-            <Group align="center" gap="xl">
-              <IconUpload size={50} />
-              <Text>Drag a file here or click to upload</Text>
-            </Group>
-          </Dropzone>
+            {file && (
+              <Text mt="sm">
+                <IconFile size={16} style={{ marginRight: 5 }} />
+                {file.name}
+              </Text>
+            )}
 
-          {file && (
-            <Text mt="sm">
-              <IconFile size={16} style={{ marginRight: 5 }} />
-              {file.name}
-            </Text>
-          )}
+            <TextInput
+              my="md"
+              label="Authority Email"
+              placeholder="Enter authority's email"
+              value={authorityEmail}
+              onChange={(e) => setAuthorityEmail(e.currentTarget.value)}
+            />
 
-          <TextInput
-            my="md"
-            label="Authority Email"
-            placeholder="Enter authority's email"
-            value={authorityEmail}
-            onChange={(e) => setAuthorityEmail(e.currentTarget.value)}
-          />
+            <TextInput
+              my="md"
+              label="Participants Email"
+              placeholder="Enter participants' email"
+              value={participantsEmail}
+              onChange={(e) => setParticipantsEmail(e.currentTarget.value)}
+            />
 
-          <TextInput
-            my="md"
-            label="Participants Email"
-            placeholder="Enter participants' email"
-            value={participantsEmail}
-            onChange={(e) => setParticipantsEmail(e.currentTarget.value)}
-          />
+            <Textarea
+              label="Enter the prompt"
+              placeholder="Describe the template details"
+              value={prompt}
+              onChange={(e) => setPrompt(e.currentTarget.value)}
+            />
 
-          <Textarea
-            label="Enter the prompt"
-            placeholder="Describe the template details"
-            value={prompt}
-            onChange={(e) => setPrompt(e.currentTarget.value)}
-          />
-
-          <Button onClick={handleSubmit} disabled={loading} fullWidth mt="md">
-            {loading ? <Loader size="sm" /> : "Process"}
-          </Button>
-        </>
-      ) : (
-        <Card
-          shadow="sm"
-          mt={40}
-          padding="lg"
-          withBorder
-          style={{
-            textAlign: "center",
-          }}
-        >
-          <Text>
-            <ThemeIcon radius="xl" size="xl" color={COLORS.green}>
-              <IconCheck size="1.5rem" />
-            </ThemeIcon>
-          </Text>
-
-          <Text size="lg" fw={700} c={COLORS.green} mt="md">
-            Your agreement generation has started successfully!
-          </Text>
-
-          <Text size="sm" mt="sm">
-            📨 You will receive an email within a minute with a link to approve
-            the agreement.
-          </Text>
-
-          <Text size="lg" fw={700} c={COLORS.blue} mt="md">
-            The email link will be valid for <strong>5 minutes</strong>. Please
-            approve it within this time.
-          </Text>
-
-          <Text size="lg" fw={600} c={COLORS.blue} mt="md">
-            ✅ Next Steps:
-          </Text>
-
-          <Text
-            size="md"
-            c={colorScheme === "dark" ? COLORS.grayDark : COLORS.grayLight}
+            <Button onClick={handleSubmit} disabled={loading} fullWidth mt="md">
+              {loading ? <Loader size="sm" /> : "Process"}
+            </Button>
+          </>
+        ) : (
+          <Card
+            shadow="sm"
+            mt={40}
+            padding="lg"
+            withBorder
+            style={{
+              textAlign: "center",
+            }}
           >
-            Open the email from us 📧 <br />
-            Click the approval link 🔗 <br />
-            Confirm the agreement to move forward ✅ <br />
-            Receive the digitally signed document 📄
-          </Text>
-        </Card>
-      )}
-    </Box>
+            <Text>
+              <ThemeIcon radius="xl" size="xl" color={COLORS.green}>
+                <IconCheck size="1.5rem" />
+              </ThemeIcon>
+            </Text>
+
+            <Text size="lg" fw={700} c={COLORS.green} mt="md">
+              Your agreement generation has started successfully!
+            </Text>
+
+            <Text size="sm" mt="sm">
+              📨 You will receive an email within a minute with a link to
+              approve the agreement.
+            </Text>
+
+            <Text size="lg" fw={700} c={COLORS.blue} mt="md">
+              The email link will be valid for <strong>5 minutes</strong>.
+              Please approve it within this time.
+            </Text>
+
+            <Text size="lg" fw={600} c={COLORS.blue} mt="md">
+              ✅ Next Steps:
+            </Text>
+
+            <Text
+              size="md"
+              c={colorScheme === "dark" ? COLORS.grayDark : COLORS.grayLight}
+            >
+              Open the email from us 📧 <br />
+              Click the approval link 🔗 <br />
+              Confirm the agreement to move forward ✅ <br />
+              Receive the digitally signed document 📄
+            </Text>
+          </Card>
+        )}
+      </Box>
+    </>
   );
 }
