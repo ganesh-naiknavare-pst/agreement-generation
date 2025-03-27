819cc8ef06fc7258a7f0733a61f15d75796d01e9
gnsaruna-bhargavi-pst <gnsaruna.bhargavi@positsource.com>
2025-03-27 12:28:54 +0530

    Created different component for furniture and appliances

--- front/src/pages/AgreementGenerator.tsx
+++ front/src/pages/AgreementGenerator.tsx
@@ -15,14 +15,12 @@ import {
   Title,
   Divider,
   Container,
-  Table,
-  ActionIcon,
   Radio,
   MultiSelect,
   Select,
   Stack,
 } from "@mantine/core";
-import { IconCheck, IconTrash } from "@tabler/icons-react";
+import { IconCheck } from "@tabler/icons-react";
 import { useForm } from "@mantine/form";
 import { DatePickerInput } from "@mantine/dates";
 import { COLORS } from "../colors";
@@ -39,7 +37,7 @@ import {
   getDefaultOtpState,
 } from "../types/otp";
 import { AddressForm } from "./AddressForm";
-
+import { FurnitureAppliances } from "./FurnitureAppliances";
 export function AgreementGenerator() {
   const { user } = useUser();
   const [active, setActive] = useState(0);
@@ -519,8 +517,8 @@ export function AgreementGenerator() {
     switch (field) {
       case "flatFloor":
         if (!/^[\w\s/,.-]+$/.test(value)) {
-            error = "Flat No. & Floor should contain valid characters only";
-        }    
+          error = "Flat No. & Floor should contain valid characters only";
+        }
         break;
       case "buildingName":
         if (!/^[a-zA-Z0-9\s]+$/.test(value)) {
@@ -878,79 +876,12 @@ export function AgreementGenerator() {
               </Radio.Group>
               {(furnishingType === "furnished" ||
                 furnishingType === "semi-furnished") && (
-                <Box>
-                  <Group gap={30}>
-                    <TextInput
-                      label="Furniture and Appliances"
-                      placeholder="Enter furniture name"
-                      key={form.key("furnitureName")}
-                      style={{ textAlign: "start", flex: 1, minWidth: 200 }}
-                      {...form.getInputProps("furnitureName")}
-                      withAsterisk
-                    />
-                    <NumberInput
-                      label="Quantity"
-                      min={1}
-                      key={form.key("furnitureQuantity")}
-                      style={{ textAlign: "start", flex: 1, minWidth: 200 }}
-                      {...form.getInputProps("furnitureQuantity")}
-                      withAsterisk
-                    />
-                    <Button
-                      style={{ marginTop: 40, marginBottom: 20 }}
-                      onClick={addFurniture}
-                    >
-                      Add
-                    </Button>
-                  </Group>
-                  {furnitureList.length > 0 && (
-                    <Card
-                      shadow="sm"
-                      padding="lg"
-                      withBorder
-                      style={{ textAlign: "center" }}
-                    >
-                      <Table
-                        highlightOnHover
-                        verticalSpacing="md"
-                        horizontalSpacing={20}
-                        mt="md"
-                      >
-                        <Table.Thead>
-                          <Table.Tr>
-                            <Table.Th>Sr No.</Table.Th>
-                            <Table.Th>Item</Table.Th>
-                            <Table.Th>Number of units</Table.Th>
-                            <Table.Th>Action</Table.Th>
-                          </Table.Tr>
-                        </Table.Thead>
-                        <Table.Tbody>
-                          {furnitureList.map((item, index) => (
-                            <Table.Tr key={index}>
-                              <Table.Td style={{ textAlign: "left" }}>
-                                {index + 1}
-                              </Table.Td>
-                              <Table.Td style={{ textAlign: "left" }}>
-                                {item.name}
-                              </Table.Td>
-                              <Table.Td style={{ textAlign: "left" }}>
-                                {item.quantity}
-                              </Table.Td>
-                              <Table.Td style={{ textAlign: "left" }}>
-                                <ActionIcon
-                                  color="red"
-                                  onClick={() => removeFurniture(index)}
-                                >
-                                  <IconTrash size={16} />
-                                </ActionIcon>
-                              </Table.Td>
-                            </Table.Tr>
-                          ))}
-                        </Table.Tbody>
-                      </Table>
-                    </Card>
-                  )}
-                </Box>
+                <FurnitureAppliances
+                  furnitureList={furnitureList}
+                  form={form}
+                  addFurniture={addFurniture}
+                  removeFurniture={removeFurniture}
+                />
               )}
             </Stack>
           </Stepper.Step>
